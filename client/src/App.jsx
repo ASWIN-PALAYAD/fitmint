4faa7ebce73594ecb01276b7169a637764ed6085
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2025-06-15 19:04:49 +0530

    admin and shop routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,40 @@ import './App.css'
 import AuthLayout from './components/auth/layout'
 import AuthLogin from './pages/auth/login'
 import AuthRegister from './pages/auth/register'
+import AdminLayout from './components/admin-view/layout'
+import AdminDashboard from './pages/admin-view/dashboard'
+import AdminProducts from './pages/admin-view/products'
+import AdminOrders from './pages/admin-view/orders'
+import AdminFeatures from './pages/admin-view/features'
+import ShoppingLayout from './components/shopping-view/layout'
+import NotFound from './pages/not-found'
+import ShoppingAccount from './pages/shopping-view/account'
+import ShoppingHome from './pages/shopping-view/home'
+import ShoppingListing from './pages/shopping-view/listing'
+import ShoppingCheckout from './pages/shopping-view/checkout'
 
 function App() {
 
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
-      <h1>Headder component</h1>
       <Routes>
         <Route path='/auth' element={<AuthLayout/>}>
             <Route path='login' element={<AuthLogin/>}/>
             <Route path='register' element={<AuthRegister/>}/>
         </Route>
+        <Route path='/admin' element={<AdminLayout/>}>
+            <Route path='dashboard' element={<AdminDashboard/>}/>
+            <Route path='products' element={<AdminProducts/>}/>
+            <Route path='orders' element={<AdminOrders/>}/>
+            <Route path='features' element={<AdminFeatures/>}/>
+        </Route>
+        <Route path='/shop' element={<ShoppingLayout/>}>
+            <Route path='account' element={<ShoppingAccount/>}/>
+            <Route path='home' element={<ShoppingHome/>}/>
+            <Route path='listing' element={<ShoppingListing/>}/>
+            <Route path='checkout' element={<ShoppingCheckout/>}/>
+        </Route>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   )
---
+++ client/src/components/shopping-view/header.jsx
@@ -0,0 +1,10 @@
+
+const ShoppingHeader = () => {
+  return (
+    <div >
+        ShoppingHeader
+    </div>
+  )
+}
+
+export default ShoppingHeader
